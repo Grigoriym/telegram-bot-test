b88410e781d633c72c796bf3c7b55e236d480f4c
Mikhalchuk Grigoriy <mikhalchukgrigoriy@yandex.ru>
2018-10-15 14:33:59 +0600

    Some fixes, added new features.

---
+++ src/main/java/com/grappim/builders/KeyboardMarkupBuilder.java
@@ -0,0 +1,9 @@
+package com.grappim.builders;
+
+/**
+ * IDE: IntelliJ IDEA Created by grigo on Oct, 15, 2018 Project: telegram-bot-test
+ */
+
+public class KeyboardMarkupBuilder {
+
+}
--- src/main/java/com/grappim/constant/FieldConstants.java
+++ src/main/java/com/grappim/constant/FieldConstants.java
@@ -6,14 +6,22 @@ package com.grappim.constant;
 
 public class FieldConstants {
 
-  public static final String OCA_OCP = "OCA/OCP";
+  public static final String OCA_OCP_BOOKS = "OCA/OCP Books";
   public static final String TDD = "TDD";
   public static final String JAVA_CORE = "JAVA CORE";
   public static final String JAVA_FX = "JAVA FX";
   public static final String SCALA = "Scala";
   public static final String KOTLIN = "Kotlin";
-  public static final String PSEUDO_RANDOM = "Pseudo random";
-  public static final String LIST_OF_QUESTIONS = "List of questions";
+  public static final String PSEUDO_RANDOM_QUESTION = "Pseudo random question";
+  public static final String LIST_OF_OCA_QUESTIONS = "List of OCA questions";
+  public static final String TESTS = "Tests";
+  public static final String BOOKS = "Books";
+  public static final String ARTICLES = "Articles";
+  public static final String WEBSITES = "Websites";
+
+  public static final String OCA_TEST = "OCA test";
+  public static final String OCP_TEST = "OCP test";
+  public static final String SPRING_TEST = "Spring test";
 
   public static final String BOOKS_COMMAND = "/books";
   public static final String ABOUT_ME_COMMAND = "/aboutme";
@@ -25,8 +33,16 @@ public class FieldConstants {
 
   public static final String SEND_ME_RANDOM_ARTICLE = "/send-random-article";
 
-  public static final String NEXT_RANDOM_QUESTION = ":arrow_right: Next Question";
   public static final String MAIN_PAGE = ":house: Main Page";
+  public static final String NEXT_RANDOM_QUESTION = ":arrow_right: Next Question";
+
+  public static final String OCA_LIST_ARROW_LEFT = "oca list arrow left";
+  public static final String OCA_LIST_ARROW_RIGHT = "oca list arrow right";
+
+  public static final String EMPTY_COMMAND = "empty command";
+
+  public static final String OCA_RANDOM_QUESTION_ANSWER_PREFIX = "03jknvwFHk";
+  public static final String OCA_NOT_RANDOM_QUESTION_PREFIX = "9nf2gtd7Figgoyi3";
 
 
 }
